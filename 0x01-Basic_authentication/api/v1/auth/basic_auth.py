d758cc15f1c65bf5238c0d7fffd41fe5a7458a9d
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-08 13:57:04 +0000

    Extract base 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class that inherits fro Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        A method to that returns the base64 part of the auhtorizarion header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        extracted = authorization_header.split(' ', 1)[1]
+        return extracted
