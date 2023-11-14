343a47d675e08eb038fb4a71fa8d11c6d5ca7294
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-14 11:04:50 +0300

    Try with validations

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,6 +32,8 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Method to add user"""
+        if not email and not hashed_password:
+            return
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
