f4c4a7d8b2fee19c67a3bedd4bc7e94403d625ce
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-14 10:49:35 +0300

    Pycodestyle check

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email, hashed_password) -> User:
         """Method to add user"""
         new_user = User(email=email, hashed_password=hashed_password)
