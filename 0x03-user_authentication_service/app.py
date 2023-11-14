afe532aafcb2918bb6be38fc40b007dfd6369258
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-15 00:33:56 +0300

    Add user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,5 +54,19 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """Method to find user and respond with 200 HTTP Status"""
+    sess_id = request.cookies.get("session_id")
+    if not sess_id:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(sess_id)
+    if not user:
+        abort(403)
+
+    return jsonify({"email": f"{user.email}"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
