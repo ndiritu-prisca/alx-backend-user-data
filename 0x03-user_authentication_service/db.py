106bc984cc211308c958680a6980d6daf4579d7c
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-14 17:56:04 +0300

    Update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,3 +51,16 @@ class DB:
             raise
         except InvalidRequestError as e:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Method that takes as argument a required user_id integer and
+        arbitrary keyword arguments, and returns None"""
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+
+        self._session.commit()
